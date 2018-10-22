6163214fc9483ee97b53625ea20c2e36548e3765
Louell Sala <louell_sala@yahoo.com>
2018-10-22 12:51:39 -0400

    first commit

---
+++ optrack.js
@@ -0,0 +1,38 @@
+$("#cont").hide(0)
+$("#cont").show(2000)
+
+var btn = document.querySelector("#btn1")
+
+btn.onclick = function () {
+  var s = document.querySelector("#a1")
+  var t = document.querySelector("#a2")
+  var u = document.querySelector("#a3")
+  var d = document.querySelector("#a4")
+  var result = document.querySelector("#result")
+
+result.innerHTML = "Verified"
+}
+
+var btn = document.querySelector("#btn2")
+
+btn.onclick = function () {
+  var s = document.querySelector("#b1")
+  var t = document.querySelector("#b2")
+  var u = document.querySelector("#b3")
+  var d = document.querySelector("#b4")
+  var result = document.querySelector("#resul")
+
+result.innerHTML = "Verified"
+}
+
+var btn = document.querySelector("#btn3")
+
+btn.onclick = function () {
+  var s = document.querySelector("#c1")
+  var t = document.querySelector("#c2")
+  var u = document.querySelector("#c3")
+  var d = document.querySelector("#c4")
+  var result = document.querySelector("#res")
+
+result.innerHTML = "Verified"
+}
